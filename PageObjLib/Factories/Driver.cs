58dc89bb48beb44ce0de13e4632f551346b6a6ca
matveykylak <matveykylak@gmail.com>
2024-06-13 23:14:01 +0300

    Add project files.

---
+++ PageObjLib/Factories/Driver.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace PageObjLib.Factories
+{
+    public static class Driver
+    {
+        private static IWebDriver? _driver;
+        private static WebDriverWait? _wait;
+        private static ChromeOptions? _chromeOptions;
+
+        public static WebDriverWait GetWait() => _wait ??= new(_driver, TimeSpan.FromSeconds(60));
+        public static IWebDriver GetDriver() => _driver ??= new ChromeDriver(GetOptions());
+        public static ChromeOptions GetOptions()
+        {
+            if (_chromeOptions == null)
+            {
+                _chromeOptions = new ChromeOptions();
+                _chromeOptions.AddArgument("start-maximized");
+            }
+
+            return _chromeOptions;
+        }
+        public static void QuitDriver()
+        {
+            _driver?.Quit();
+            _driver = null;
+        }
+        public static void QuitWait()
+        {
+            _wait = null;
+        }
+        public static void DisposeDriver() => _driver?.Dispose();
+    }
+}
---
+++ PageObjLib/Pages/Page.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using PageObjLib.Factories;
+using SeleniumExtras.WaitHelpers;
+
+namespace PageObjLib.Pages
+{
+    public static class Page
+    {
+        private static Actions? _action;
+
+        public static void GoUrl(string url) => Driver.GetDriver().Navigate().GoToUrl(url);
+        public static IWebElement GetElement(By locator)
+        {
+            return Driver.GetWait().Until(ExpectedConditions.ElementToBeClickable(locator));
+        }
+        public static List<IWebElement> GetListOfElements(By locator)
+        {
+            return Driver.GetWait().Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(locator)).ToList();
+
+        }
+        public static Dictionary<string, IWebElement> GetDictionaryOfElements(By locator)
+        {
+            var elements = Driver.GetWait().Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(locator)).DistinctBy(a => a.Text).ToList();
+            return elements.ToDictionary(p => p.Text, p => p);
+        }
+        public static Actions GetActions() => _action ??= new Actions(Driver.GetDriver());
+        public static void QuitActions()
+        {
+            _action = null;
+        }
+    }
+}
